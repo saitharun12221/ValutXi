2969035053b82269d29e8a96ea3ce9b7feced4bb
saitharun12221 <saitharunpetla@gmail.com>
2025-11-26 11:00:00 +0530

    add new project

---
+++ vaultX/src/test/java/com/example/vaultX/VaultXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vaultX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
